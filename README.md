9b78b2891310df8a3444b069d1a4a728bd7f1b67
Icaro <danilo.oliva153@gmail.com>
2025-12-15 22:45:59 -0300

    Primera version funcional del rastreador

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ecommerce-price-tracker
+# 📉 Hardware Price Tracker
+
+Bot automatizado en Python que rastrea precios de productos en MercadoLibre Argentina para detectar ofertas.
+
+## 🚀 Funcionalidades
+- **Scraping Web:** Extrae título y precio en tiempo real.
+- **Data Cleaning:** Convierte precios de texto a números enteros.
+- **Persistencia:** Guarda un historial de precios en un archivo CSV (`precios.csv`).
+- **Alertas:** Notifica en consola si el precio baja de un umbral objetivo.
+
+## 🛠️ Tecnologías
+- Python 3
+- BeautifulSoup4
+- Requests
+- CSV Module
+
+## 📦 Instalación
+1. Clonar el repositorio.
+2. Instalar dependencias:
+   ```bash
+   pip install -r requirements.txt
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from datetime import datetime
+import csv
 
 os.system("cls")
+
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"}
 
 respuesta = requests.get("https://www.mercadolibre.com.ar/auriculares-nothing-ear-a-bluetooth-53-con-cancelacion-de-ruido-de-45-db-integracion-con-chat-gpt-audio-hi-res-color-blanco/p/MLA38365682#reco_item_pos=3&reco_backend=item_decorator&reco_backend_type=function&reco_client=home_items-decorator-legacy&reco_id=f0d60d7b-05d9-4200-b1da-978da6da1dfc&reco_model=&c_id=/home/navigation-recommendations-seed/element&c_uid=f310f6ec-2f22-4f6e-8091-40655fe92052&da_id=navigation&da_position=4&id_origin=/home/dynamic_access&da_sort_algorithm=ranker", headers=headers)
-
+  
 sopa = BeautifulSoup(respuesta.content, 'html.parser')
 
 etiqueta_titulo = sopa.find("h1", class_="ui-pdp-title")
@@ -23,15 +25,20 @@ precio = etiqueta_precio.get_text(strip=True).replace('.', '')
 precio = int(precio)
 
 #extraemos hora
-fecha_de_scraping = datetime.now()
+fecha_de_scraping = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+#guardamos hora
+with open("precios.csv", "a", newline='') as archivo_precios:
+  escritor = csv.writer(archivo_precios)
+  escritor.writerow([fecha_de_scraping, texto_titulo, precio])
+
 
 print(texto_titulo)
 
 
 if precio < 180000:
-  print(f"Oferta!Comprar ya. Precio: ${precio}")
+  print(f"\nOferta!Comprar ya. Precio: ${precio}")
 else:
-  print("Seguir esperando")
+  print(f"\nSeguir esperando. Precio: {precio}")
 
 print(f"Fecha = {fecha_de_scraping}")
-  
